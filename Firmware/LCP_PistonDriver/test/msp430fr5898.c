23df7ffb35137e7e0a627f43debb709df6b5232e
MCasari-PMEL <matthew.casari@noaa.gov>
2021-02-05 12:34:22 -0800

    Adding missing files

---
+++ Firmware/LCP_PistonDriver/test/msp430fr5898.c
@@ -0,0 +1,123 @@
+#include "msp430fr5994.h"
+
+
+/** UC Ports */
+uint16_t UCA0BRW = 0;
+uint16_t UCA0CTLW0 = 0;
+uint16_t UCA0CTLW1 = 0;
+uint16_t UCA0MCTLW = 0;
+uint16_t UCA0IE = 0;
+uint16_t UCA0STATW = 0;
+uint16_t UCA0TXBUF = 0;
+uint16_t UCA0RXBUF = 0;
+
+uint16_t UCA1BRW = 0;
+uint16_t UCA1CTLW0 = 0;
+uint16_t UCA1CTLW1 = 0;
+uint16_t UCA1MCTLW = 0;
+uint16_t UCA1IE = 0;
+uint16_t UCA1STATW = 0;
+uint16_t UCA1TXBUF = 0;
+uint16_t UCA1RXBUF = 0;
+
+uint16_t UCA2BRW = 0;
+uint16_t UCA2CTLW0 = 0;
+uint16_t UCA2CTLW1 = 0;
+uint16_t UCA2MCTLW = 0;
+uint16_t UCA2IE = 0;
+uint16_t UCA2STATW = 0;
+uint16_t UCA2TXBUF = 0;
+uint16_t UCA2RXBUF = 0;
+
+uint16_t UCA3BRW = 0;
+uint16_t UCA3CTLW0 = 0;
+uint16_t UCA3CTLW1 = 0;
+uint16_t UCA3MCTLW = 0;
+uint16_t UCA3IE = 0;
+uint16_t UCA3STATW = 0;
+uint16_t UCA3TXBUF = 0;
+uint16_t UCA3RXBUF = 0;
+
+
+/** GPIO Ports and Settings */
+uint16_t P1OUT = 0;
+uint16_t P2OUT = 0;
+uint16_t P3OUT = 0;
+uint16_t P4OUT = 0;
+uint16_t P5OUT = 0;
+uint16_t P6OUT = 0;
+uint16_t P7OUT = 0;
+uint16_t P8OUT = 0;
+
+uint16_t P1IN = 0;
+uint16_t P2IN = 0;
+uint16_t P3IN = 0;
+uint16_t P4IN = 0;
+uint16_t P5IN = 0;
+uint16_t P6IN = 0;
+uint16_t P7IN = 0;
+uint16_t P8IN = 0;
+
+uint16_t P1DIR = 0;
+uint16_t P2DIR = 0;
+uint16_t P3DIR = 0;
+uint16_t P4DIR = 0;
+uint16_t P5DIR = 0;
+uint16_t P6DIR = 0;
+uint16_t P7DIR = 0;
+uint16_t P8DIR = 0;
+
+uint16_t P1SEL0 = 0;
+uint16_t P2SEL0 = 0;
+uint16_t P3SEL0 = 0;
+uint16_t P4SEL0 = 0;
+uint16_t P5SEL0 = 0;
+uint16_t P6SEL0 = 0;
+uint16_t P7SEL0 = 0;
+uint16_t P8SEL0 = 0;
+
+uint16_t P1SEL1 = 0;
+uint16_t P2SEL1 = 0;
+uint16_t P3SEL1 = 0;
+uint16_t P4SEL1 = 0;
+uint16_t P5SEL1 = 0;
+uint16_t P6SEL1 = 0;
+uint16_t P7SEL1 = 0;
+uint16_t P8SEL1 = 0;
+
+uint16_t P1REN = 0;
+uint16_t P2REN = 0;
+uint16_t P3REN = 0;
+uint16_t P4REN = 0;
+uint16_t P5REN = 0;
+uint16_t P6REN = 0;
+uint16_t P7REN = 0;
+uint16_t P8REN = 0;
+
+uint16_t P1IFG = 0;
+uint16_t P2IFG = 0;
+uint16_t P3IFG = 0;
+uint16_t P4IFG = 0;
+uint16_t P5IFG = 0;
+uint16_t P6IFG = 0;
+uint16_t P7IFG = 0;
+uint16_t P8IFG = 0;
+
+uint16_t P1IES = 0;
+uint16_t P2IES = 0;
+uint16_t P3IES = 0;
+uint16_t P4IES = 0;
+uint16_t P5IES = 0;
+uint16_t P6IES = 0;
+uint16_t P7IES = 0;
+uint16_t P8IES = 0;
+
+uint16_t P1IE = 0;
+uint16_t P2IE = 0;
+uint16_t P3IE = 0;
+uint16_t P4IE = 0;
+uint16_t P5IE = 0;
+uint16_t P6IE = 0;
+uint16_t P7IE = 0;
+uint16_t P8IE = 0;
+
