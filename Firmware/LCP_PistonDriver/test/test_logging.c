8dc2b9a11f7c38ea34e674afacb03352c7ac5ce6
mcasari-PMEL <matthew.casari@noaa.gov>
2021-06-30 12:20:35 -0400

    initial logging commit

---
+++ Firmware/LCP_PistonDriver/test/test_logging.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "logging.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_logging_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement logging");
+}
+
+#endif // TEST
