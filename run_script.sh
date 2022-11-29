5ab3dfe4c6bd80c606f3ef6bfd77d49fc779fca8
Basharat, Basharat <basharat.martin@noaa.gov>
2022-11-28 21:51:52 -0500

    run_script.sh : compile mspdebug from source, added more checks

--- run_script.sh
+++ run_script.sh
@@ -24,33 +24,67 @@ fi
 
 ## extract msp430-gcc tar file 
 echo "Extracting msp430-gcc.tar.xz file ..."
-tar xf ./msp430-gcc.tar.xz -C ./
-chown $USER:$USER -R ./msp430-gcc
+su $USER -c "tar xf ./msp430-gcc.tar.xz -C ../"
 
 ## copy libmsp430.so file to the /usr/local/lib/
 echo "Copying libmsp430.so file to /usr/local/lib ..."
-cp ./msp430-gcc/libmsp430/libmsp430.so /usr/local/lib/
+cp ../msp430-gcc/libmsp430/libmsp430.so /usr/local/lib/
 sleep 1
 
 ## install required packages
 echo "Updating the repositories ..."
 apt -qq update
 sleep 1
-apt install -y libboost-filesystem1.71.0 libboost-chrono1.71.0 libboost-thread1.71.0 libusb-1.0-0 libudev1
+apt install --no-install-recommends -y libusb-dev libusb-0.1-4 libudev-dev make build-essential g++ 
 sleep 1
+
+## compile mspdebug (Flasher)
+echo "Git cloning mspdebugger ..."
+su $USER -c "git clone https://github.com/dlbeer/mspdebug.git"
+su $USER -c "mv mspdebug ../"
+echo "Compiling mspdebugger ..."
+sleep 1
+su $USER -c "make -C ../mspdebug/"
+su $USER -c "cp ../mspdebug/mspdebug ../msp430-gcc/bin/msp430-elf-debug"
 
 PWD=$(pwd)
 
 ## export the GCC PATH
 echo "Exporting GCC PATH into .bashrc file ..."
 
-echo "export PATH=\"$PWD/msp430-gcc/bin:\$PATH\"" >> /home/$USER/.bashrc
-echo "export LD_PRELOAD=\"/usr/local/lib/libmsp430.so\"" >> /home/$USER/.bashrc
-echo "export GCC_MSP_ROOT=\"$PWD/msp430-gcc\"" >> /home/$USER/.bashrc
+if [ $(grep -r "GCC_MSP_ROOT" /home/$USER/.bashrc > /dev/null; echo $?) -eq 0 ]
+then 
+	echo "GCC PATH already present"
+else
+	echo "export PATH=\"$PWD/../msp430-gcc/bin:\$PATH\"" >> /home/$USER/.bashrc
+	echo "export LD_PRELOAD=\"/usr/local/lib/libmsp430.so\"" >> /home/$USER/.bashrc
+	echo "export GCC_MSP_ROOT=\"$PWD/../msp430-gcc\"" >> /home/$USER/.bashrc
+fi
+
+echo "Removing unnecessary files ..."
+sleep 1
+su $USER -c "rm -rf ../mspdebug"
+
+echo "Checking if a user in the dialout group ..."
+if [ $(groups $USER | grep dialout >/dev/null; echo $?) -eq 0 ]
+then
+	echo "User in already in the dialout group ..."
+	LOGOUT_WARNING=0
+else
+	echo "User is not in the dialout group, Adding into the dialout group ..."
+	usermod -a -G dialout $USER
+	LOGOUT_WARNING=1
+fi
+sleep 1
 
 echo "completed"
 sleep 1
 echo "\n"
 echo "Please run the command: \"source ~/.bashrc\""
 echo "\n"
+
+if [ $LOGOUT_WARNING -eq 1 ]
+then
+	echo "Please log out and log in again to activate user to dialout group"
+fi
 
